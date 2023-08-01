b5f8f28616fe93692bcada249690399f88add600
Sergey Stolyarchuk <dcelburrito@gmail.com>
2023-08-01 17:02:07 +0200

    feat: проектная работа №14

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cardsRouter = require('./routes/cards');
 const signinRouter = require('./routes/signin');
 const signupRouter = require('./routes/signup');
 const errorHandler = require('./middlewares/errorHandler');
+const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
 
 const { PORT } = process.env;
@@ -20,7 +21,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
 
 app.use('/signin', signinRouter);
 app.use('/signup', signupRouter);
-
+app.use(auth);
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 app.use('*', (req, res, next) => {
