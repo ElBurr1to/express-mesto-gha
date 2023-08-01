a472b274bbbb159a80c7ad2302cdb9674405e4d1
Sergey Stolyarchuk <dcelburrito@gmail.com>
2023-08-01 17:59:28 +0200

    feat: проектная работа №14

--- routes/signup.js
+++ routes/signup.js
@@ -8,7 +8,9 @@ router.post('/', celebrate({
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string().uri({
+      scheme: ['http', 'https'],
+    }),
   }),
 }), createUser);
 
