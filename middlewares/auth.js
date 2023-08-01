870b35391c5904fab254eef8b58b3ac803fc72d7
Sergey Stolyarchuk <dcelburrito@gmail.com>
2023-08-02 01:26:51 +0200

    feat: проектная работа №14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ const AuthorizationError = require('../errors/AuthorizationError');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new AuthorizationError('Необходима авторизация1111'));
+    next(new AuthorizationError(authorization));
   }
 
   const token = authorization.replace('Bearer ', '');
