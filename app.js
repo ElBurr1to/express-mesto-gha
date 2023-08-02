9f475d4d4f9e8367a805701f4f39fde62be3dd8a
Sergey Stolyarchuk <dcelburrito@gmail.com>
2023-08-02 13:13:21 +0200

    fix: исправления

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
 
 app.use('/signin', signinRouter);
 app.use('/signup', signupRouter);
-app.use('/users', auth, usersRouter);
-app.use('/cards', auth, cardsRouter);
+app.use(auth);
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Неправильный путь'));
 });
